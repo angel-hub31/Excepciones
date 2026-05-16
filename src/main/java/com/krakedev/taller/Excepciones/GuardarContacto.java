5b17fd7eb95220339b9650fc9e7f76de41d72f21
angel-hub31 <moralesmales1@hotmail.com>
2026-05-15 21:31:17 -0500

    se crea la clase GuardarContacto,tambien usamos try catch

---
+++ src/main/java/com/krakedev/taller/Excepciones/GuardarContacto.java
@@ -0,0 +1,38 @@
+package com.krakedev.taller.Excepciones;
+
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GuardarContacto {
+	private static final Logger log = LoggerFactory.getLogger(GuardarContacto.class);
+	
+	public static void guardar(String nombre,String apellido,String telefono) {
+		FileWriter escritor =null;
+		
+		try {
+			escritor=new FileWriter("contactos.txt",true);
+			
+			escritor.write("nombre\n");
+			escritor.write("apellido\n");
+            escritor.write("telefono \n");
+            
+            log.info("Contacto guardado exitosamente: {} {}", nombre, apellido);
+			
+		}catch(IOException e) {
+			log.error("Error al intentar guardar el contacto: ", e);
+		}finally {
+			if (escritor !=null) {
+				try {
+					escritor.close();
+				}catch(IOException e) {
+					log.error("Error: ",e);
+				}
+			}
+		}
+	}
+}
