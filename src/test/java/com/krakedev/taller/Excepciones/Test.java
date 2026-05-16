de29fad6d5b126fd78eb189e9e55a065fc09cc68
angel-hub31 <moralesmales1@hotmail.com>
2026-05-15 21:09:22 -0500

    Primer commit

---
+++ src/test/java/com/krakedev/taller/Excepciones/Test.java
@@ -0,0 +1,22 @@
+package com.krakedev.taller.Excepciones;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+
+	private static final Logger log = LoggerFactory.getLogger(Test.class);
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 0;
+		try {
+			int c = a / b;
+			log.info("Resultado es: " + c);
+		} catch (Exception e) {
+			log.error("error matematico: " + e.getMessage());
+
+		}
+
+	}
+}
